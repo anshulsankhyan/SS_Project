3327109ff73db83c308dc01b73a9e04a454dd166
Dhruv <dhruv.kharkwal@mobisy.com>
2023-07-24 20:41:30 +0530

    first

---
+++ helper.h
@@ -0,0 +1,16 @@
+#ifndef HELPER
+#define HELPER
+
+// Admin Credientials
+#define ADMIN_LOGIN_ID "admin"
+#define ADMIN_PASSWORD "pass"
+
+// Database files
+#define ACCOUNT_FILE "./account.bank"
+#define CUSTOMER_FILE "./customer.bank"
+#define TRANSACTION_FILE "./transactions.bank"
+
+// Customer's by default password
+#define CUSTOMER_DEFAULT_PASSWORD "temp"
+
+#endif
---
+++ transaction.h
@@ -0,0 +1,15 @@
+#ifndef TRANSACTIONS
+#define TRANSACTIONS
+
+#include <time.h>
+
+struct Transaction {
+    int transactionId;  // Transaction's id
+    int accountNumber;  // Account number related to the transaction
+    bool operation; // 0 -> withdraw, 1 -> deposit
+    long int oldBalance;    // Balance before the transaction
+    long int newBalance;    // Balance after the transaction
+    time_t transactionTime; // Time of the transaction
+};
+
+#endif
